d8ede2d3943e4a4c1c552a8557e5d8498adbb95e
Qi Fan <26087110+fanq15@users.noreply.github.com>
2021-04-26 11:41:15 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,2 @@
-ID=3001
-CUDA_VISIBLE_DEVICES=0 python test.py --model GICD --param_root tmp/GICD_run1 --save_root /home/fanqi/data/SalMaps/$ID
-CUDA_VISIBLE_DEVICES=0 python eval-co-sod/main.py --pred_dir /home/fanqi/data/SalMaps/$ID --gt_dir /home/fanqi/data/sod/cosod/gt/
+CUDA_VISIBLE_DEVICES=0 python test.py --model GCoNet --param_root tmp/GCoNet_run1 --save_root ./data/SalMaps/
+CUDA_VISIBLE_DEVICES=0 python eval-co-sod/main.py --pred_dir ./data/SalMaps/ --gt_dir ./data/sod/cosod/gt/
