85cc188eeb4aef93386e4e89221637aafc81ef5c
Qi Fan <26087110+fanq15@users.noreply.github.com>
2021-04-26 11:42:15 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
-CUDA_VISIBLE_DEVICES=0 python test.py --model GCoNet --param_root tmp/GCoNet_run1 --save_root ./data/SalMaps/
-CUDA_VISIBLE_DEVICES=0 python eval-co-sod/main.py --pred_dir ./data/SalMaps/ --gt_dir ./data/sod/cosod/gt/
+CUDA_VISIBLE_DEVICES=0 python test.py --model GCoNet --param_root tmp/GCoNet_run1 --save_root ./data/SalMaps
+CUDA_VISIBLE_DEVICES=0 python eval-co-sod/main.py --pred_dir ./data/SalMaps --gt_dir ./data/gts
